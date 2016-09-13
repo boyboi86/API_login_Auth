451db98a6b8d74fae6350cdc4b7bfca1b82640d2
boyboi86 <boyboi86@gmail.com>
2016-09-13 19:39:13 +0800

    employ passport strategies

---
+++ services/passport.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const user = require('../model/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup Options for jwt Strategy
+const JwtOptions = {};
+
+//create JWT Strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
+  user.findById(payload.sub, function(err, user){
+    if(err) {
+      return done(err, false);
+    }
+    if(user){
+       done(null, user)
+    } else {
+      done(null, false)
+    }
+  })
+});
+
+//Tell Passport to use strategy
