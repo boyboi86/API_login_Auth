4eb469d67dbd367dba7bc89009960a4e0cecdbd0
boyboi86 <boyboi86@gmail.com>
2016-09-13 22:11:22 +0800

    enable require auth for logins

--- router.js
+++ router.js
@@ -1,4 +1,8 @@
 const authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+//session false will disable session based cookie esp since we are using tokens
+const requireAuth = passport.authenticate('jwt', { session: false })
 
 module.exports = function (app){
   app.post('/signup', authentication.signup)
