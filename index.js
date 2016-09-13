e7841739e31be54fb9fdf7d910548a8a29b4091e
boyboi86 <boyboi86@gmail.com>
2016-09-13 15:54:33 +0800

    update mongoose connection

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 const router = require('./router.js');
+const mongoose = require('mongoose');
 
+//db set up note: when you run mongod --dbpath */data/db
+mongoose.connect('mongodb://localhost:auth/auth');
+
+//app set up
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
+//app listerners
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
