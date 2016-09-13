d4ce0f1967e9f352ae4b980e265d019161f714f3
boyboi86 <boyboi86@gmail.com>
2016-09-13 13:30:13 +0800

    update app setup

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
