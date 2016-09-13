7ccf5fd000c60060da9581e20512f5f648f8c250
boyboi86 <boyboi86@gmail.com>
2016-09-13 17:45:33 +0800

    update index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require('./router.js');
 const mongoose = require('mongoose');
 
 //db set up note: when you run mongod --dbpath */data/db
-mongoose.connect('mongodb://localhost/auth:27017');
+mongoose.connect('mongodb://localhost/auth');
 
 //app set up
 app.use(morgan('combined'));
