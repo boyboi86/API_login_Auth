278be33e209d01449bcd307e8d33fed4fc16619f
boyboi86 <boyboi86@gmail.com>
2016-09-13 17:45:08 +0800

    update authentication with email/password

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,5 +4,29 @@ module.exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  
+  if(!password || !email){
+    res.status(422).send({ error: 'must include email and password' })
+  }
+
+  user.findOne({ email: email}, function(err, existingUser){
+    if(err){
+       return next(err)
+    }
+    if(existingUser){
+      return res.status(422).send({ error: 'Email is invalid' })
+    }
+
+    const User = new user({
+      email: email,
+      password: password
+    })
+
+    User.save(function(err){
+      if(err){
+        return next(err);
+      }
+      res.json(User);
+    })
+
+  })
 }
