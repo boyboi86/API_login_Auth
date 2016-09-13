53c29d47512f670b9371fc9e1059907bb2b50ffe
boyboi86 <boyboi86@gmail.com>
2016-09-13 15:12:36 +0800

    update model

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
+const router = require('./router.js');
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
