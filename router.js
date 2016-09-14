f93c05db87d009d3f265e50738b54403a7523ff5
boyboi86 <boyboi86@gmail.com>
2016-09-14 15:01:28 +0800

    routes completed for GET & POST

--- router.js
+++ router.js
@@ -3,7 +3,12 @@ const passportService = require('./services/passport');
 const passport = require('passport');
 //session false will disable session based cookie esp since we are using tokens
 const requireAuth = passport.authenticate('jwt', { session: false })
+const requireSignin = passport.authenticate('local', { session: false })
 
 module.exports = function (app){
+  app.get('/', requireAuth, function(req, res){
+    res.json({ message: 'authentication header is there'})
+  })
+  app.post('/signin', requireSignin, authentication.signin)
   app.post('/signup', authentication.signup)
 }
